bcad4f92e659bfe52331f516c34fcf9d3268009c
RakeshYemineni1 <yrk122005@gmail.com>
2025-03-13 15:21:13 +0530

    Update

--- news.py
+++ news.py
@@ -2,10 +2,10 @@ import os
 import requests
 
 
-os.environ["NEWS_API_KEY"] = "fe0db6edadd74961920820f98a2347ed"  # Add this line temporarily for testing
+os.environ["NEWS_API_KEY"] = "Your Key"  # Add this line temporarily for testing
 API_key = os.getenv("NEWS_API_KEY")
 
 print("API Key:", API_key)  # This should now print the key
-r = requests.get(f"https://newsapi.org/v2/top-headlines?country=in&apiKey=fe0db6edadd74961920820f98a2347ed")
+r = requests.get(f"https://newsapi.org/v2/top-headlines?country=in&apiKey={API_key}")
 print("Response Code:", r.status_code)
 print("Response JSON:", r.json())  # Print full response
